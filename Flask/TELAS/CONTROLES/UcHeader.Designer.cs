7cdfe7b6b6877c31ea70890a8bf04f21d27ef59d
pollauf <matheuspollauf@hotmail.com>
2020-06-13 01:10:31 -0300

    Primeir Commit

---
+++ Flask/TELAS/CONTROLES/UcHeader.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Flask.TELAS.CONTROLES
+{
+    partial class UcHeader
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(17, 8);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(56, 24);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "Título";
+            // 
+            // UcHeader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(54)))), ((int)(((byte)(76)))));
+            this.Controls.Add(this.lblTitulo);
+            this.MaximumSize = new System.Drawing.Size(0, 40);
+            this.MinimumSize = new System.Drawing.Size(400, 40);
+            this.Name = "UcHeader";
+            this.Size = new System.Drawing.Size(400, 40);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
---
+++ Flask/TELAS/CONTROLES/UcHeader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Flask.TELAS.METODOS;
+
+namespace Flask.TELAS.CONTROLES
+{
+    public partial class UcHeader : UserControl
+    {
+        private string titulo;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set
+            {
+                titulo = value;
+                lblTitulo.Text = titulo;
+            }
+        }
+
+        public UcHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
